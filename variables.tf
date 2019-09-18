b1492204604c92c5a67f2f7af9ce3a4a30b4b9a0
unknown <arunreddyinfo@gmail.com>
2019-09-18 22:24:08 +0530

    3rd commit

--- variables.tf
+++ variables.tf
@@ -5,10 +5,10 @@ variable "vpc_cidr" {
   default = "10.20.0.0/16"
 }
 variable "subnets_cidr" {
-    type = "list"
-    default = ["10.20.1.0/24","10.20.2.0/24"]
- }
+  type    = "list"
+  default = ["10.20.1.0/24", "10.20.2.0/24"]
+}
 variable "azs" {
-    type = "list"
-    default = ["ap-south-1a","ap-south-1b"]
-  }
+  type    = "list"
+  default = ["ap-south-1a", "ap-south-1b"]
+}
--- vpc.tf
+++ vpc.tf
@@ -1,30 +1,45 @@
 # VPC for our applications
-    resource "aws_vpc" "arun_vpc" {
-    cidr_block       = "${var.vpc_cidr}"
-    tags = {
+resource "aws_vpc" "arun_vpc" {
+  cidr_block = "${var.vpc_cidr}"
+  tags = {
     Name = "Arunvpc"
   }
 }
 # create Internet Gatway attach it to arun_vpc
 resource "aws_internet_gateway" "arun_igw" {
-    vpc_id = "${aws_vpc.arun_vpc.id}"
+  vpc_id = "${aws_vpc.arun_vpc.id}"
   tags = {
     Name = "main"
   }
 }
 #Build subnets for our VPCs
 resource "aws_subnet" "public" {
-    count = "${length(var.subnets_cidr)}"
-    vpc_id     = "${aws_vpc.arun_vpc.id}"
-    availability_zone = "${element(var.azs, count.index)}"
-  cidr_block = "${element(var.subnets_cidr, count.index)}"
+  count             = "${length(var.subnets_cidr)}"
+  vpc_id            = "${aws_vpc.arun_vpc.id}"
+  availability_zone = "${element(var.azs, count.index)}"
+  cidr_block        = "${element(var.subnets_cidr, count.index)}"
 
   tags = {
     Name = "subnet-${count.index + 1}"
   }
-  
+}
+resource "aws_route_table" "public_rt" {
+  vpc_id = "${aws_vpc.arun_vpc.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.arun_igw.id}"
+  }
+  tags = {
+    Name = "Public-rt"
+  }
+}
+#Attach route table with public subnets
+resource "aws_route_table_association" "a" {
+  count          = "${length(var.subnets_cidr)}"
+  subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
+  route_table_id = "${aws_route_table.public_rt.id}"
 }
 
-  
+
 
 
